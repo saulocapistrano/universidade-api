645e49e6635164b9fc0c2ba89f02981e55868668
saulocapistrano <saulocapistrano@gmail.com>
2025-06-29 11:51:23 -0300

    chore: preparar base final para sincronizar develop e master

--- src/main/java/br/universidade/App.java
+++ src/main/java/br/universidade/App.java
@@ -1,4 +1,11 @@
 package br.universidade;
 
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
 public class App {
+    public static void main(String ... args){
+        Quarkus.run(args);
+    }
 }
